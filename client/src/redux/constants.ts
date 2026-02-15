d3063210da986126e09cad431b1a1804f38a81bb
mehmetkahraman0 <mehmetkahraman34420@gmail.com>
2026-02-15 19:48:24 +0300

    changed query const

--- client/src/redux/constants.ts
+++ client/src/redux/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000/"
+export const BASE_URL = "http://62.84.187.91:3000/"
 export const USER_URL = "api/user"
 export const ROOM_URL = "api/room"
 export const CHANEL_URL = "api/chanel"
